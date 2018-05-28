ced80b68039784d9cf98f19b99107f6e754a9b3a
Cooldude2606 <cooldude260607@gmail.com>
2018-05-28 22:16:05 +0100

    Force rename Files

--- scenario/modules/index.lua
+++ scenario/modules/index.lua
@@ -1,11 +1,11 @@
 return {
-    ['moduleOne']='/modules/moduleOne/control',
-    ['moduleTwo']='/modules/moduleTwo/control',
+    ['moduleOne']='/modules/ModuleOne/control',
+    ['moduleTwo']='/modules/ModuleTwo/control',
     ['errorHandler']='/modules/ErrorHandlers/control',
     ['Game']='/modules/StdLib/game',
     ['Color']='/modules/StdLib/color',
     ['Time']='/modules/StdLib/time',
     ['table']='/modules/StdLib/table',
     ['string']='/modules/StdLib/string',
-    ['Explib']='/modules/Explib/control',
+    ['Explib']='/modules/ExpLib/control',
 }
