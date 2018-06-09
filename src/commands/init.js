055b5319db29b310bb36c4df1ef215f561ce4e38
Cooldude2606 <cooldude260607@gmail.com>
2018-06-09 13:30:30 +0100

    Basic Info Setup

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,11 +1,6 @@
 // require 
-const readline = require('readline')
+const promptly = require('promptly')
 const fs = require('fs')
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 
 function read_default(dir,key) {
     try {
@@ -17,25 +12,28 @@ function read_default(dir,key) {
     }
 }
 
-function get_input(dir,data,data_key,options,options_key,name,default_value) {
-    data[data_key] = options[options_key] || read_default(dir,data_key) || default_value
+async function get_input(dir,data,data_key,options,options_key,name,default_value) {
+    const default_input = options[options_key] || read_default(dir,data_key) || default_value
     if (!options[options_key] && !options.yesAll) {
-        rl.question(`Module ${name}: (${data[data_key]}) `,input => data[data_key]=input)
-    }
+        data[data_key] = await promptly.prompt(`Module ${name}: (${default_input}) `,{trim:false,default:default_input})
+    } else data[data_key]=default_input
 }
 
-function basic(dir,options) {
-    const data = []
+async function basic(dir,data,options) {
     const path = fs.realpathSync(dir)
     const dir_name = path.lastIndexOf('/') > 0 && path.substring(path.lastIndexOf('/')+1) || path.lastIndexOf('\\') > 0 && path.substring(path.lastIndexOf('\\')+1) || undefined
-    get_input(dir,data,'name',options,'module_name','name',dir_name)
-    get_input(dir,data,'version',options,'version','version','1.0.0')
-    get_input(dir,data,'module',options,'module','type','Scenario')
-    get_input(dir,data,'description',options,'description','description','<blank>')
+    await get_input(dir,data,'name',options,'moduleName','name',dir_name)
+    await get_input(dir,data,'version',options,'moduleVersion','version','1.0.0')
+    await get_input(dir,data,'module',options,'module','type','Scenario')
+    await get_input(dir,data,'description',options,undefined,'description','<blank>')
 }
 
-module.exports = (dir='.',options) => {
-    const data = []
-    basic(dir,options)
-    rl.close()
+module.exports = async (dir='.',options) => {
+    try {
+        const data = []
+        await basic(dir,data,options)
+        console.log(data)
+    } catch(error) {
+        if (!error.message === 'canceled') console.log(error)
+    }
 }
